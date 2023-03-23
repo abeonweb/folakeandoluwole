f31ba8d54b4a38426f0cbcc87bec963576537112
abeonweb <bmospecific@yahoo.com>
2023-03-23 21:08:01 +0100

    shared navigation component

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "navigation.module.css"
+
+const Navigation = () => {
+  return (
+    <header>
+        //logo
+        <h1 className={styles.title}>Folake&amp;Oluwole</h1>
+        //nav with ul
+    </header>
+  )
+}
+
+export default Navigation
