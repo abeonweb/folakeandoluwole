7a943870d6238888ac34ea70a17cfa15d8303409
abeonweb <bmospecific@yahoo.com>
2023-03-24 22:26:33 +0100

    fixed path to css module

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import styles from "navigation.module.css"
+import styles from "../navigation.module.css"
 
 const Navigation = () => {
   return (
-    <header>
-        //logo
-        <h1 className={styles.title}>Folake&amp;Oluwole</h1>
-        //nav with ul
+    <header className={styles.header}>
+        {/* logo */}
+        <h1 className={styles.title}>Folake &amp; Oluwole</h1>
+        {/* nav with ul */}
     </header>
   )
 }
