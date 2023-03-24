bdd289cff761562f0a64ad22c65aaef4ef396673
abeonweb <bmospecific@yahoo.com>
2023-03-24 22:28:21 +0100

    add shared layout to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,28 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import SharedLayout from "./components/SharedLayout"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-          
-            <Image
-              src="/fo1.jpg"
-              alt="Couple Image"
-              className={styles.image}
-              width={400}
-              height={500}
-              priority
-            />
+        <SharedLayout>
+        <div className={styles.description}>
+          <div>
+            
+              <Image
+                src="/fo1.jpg"
+                alt="Couple Image"
+                className={styles.image}
+                width={400}
+                height={500}
+                priority
+                />
+          </div>
         </div>
-      </div>
-    </main>
+    </SharedLayout>
+      </main>
   )
 }
