11827c01e32673eb883a74e48fa993708a494fcc
abeonweb <bmospecific@yahoo.com>
2023-03-25 19:24:37 +0100

    location component

---
+++ app/components/Locations.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Locations = () => {
+  return (
+    <div>
+        <h1>Information of Interest</h1>
+        <div>
+            <h2>Engagement - Time</h2>
+            {/*Add a map*/}
+            {/*Additional info*/}
+        </div>
+        <div>
+            <h2>Wedding at Church - Time</h2>
+            {/*Add a map*/}
+            {/*Additional info*/}
+        </div>
+        <div>
+            <h2>Reception - Time</h2>
+            {/*Add a map*/}
+            {/*Additional info*/}
+        </div>
+    </div>
+  )
+}
+
+export default Locations
