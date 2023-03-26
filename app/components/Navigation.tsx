64d81d89e15ff3ca14d5c5a04058dbfac1e4fc0b
abeonweb <bmospecific@yahoo.com>
2023-03-26 17:47:59 +0100

    tangerine font  to embellish navigation

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import styles from "../css/navigation.module.css"
 import Link from "next/link"
+import { Tangerine } from 'next/font/google'
+
+const tangerine = Tangerine({ subsets: ['latin'], weight: ["400", "700"], variable: '--tangerine-font' })
 
 const Navigation = () => {
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${tangerine.variable}`}>
         <h1 className={styles.title}>Folake &amp; Oluwole</h1>
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
+        <nav className={styles.nav}>
+            <ul className={styles.navList}>
+                <li className={styles.navListItem}>
+                    <Link href="/"><span className={styles.linkFirstLetter}>H</span>ome</Link>
                 </li>
-                <li>
-                    <Link href="/gallery">Gallery</Link>
+                <li className={styles.navListItem}>
+                    <Link href="/gallery"><span className={styles.linkFirstLetter}>G</span>allery</Link>
                 </li>
-                <li>
-                    <Link href="#location">Locations</Link>
+                <li className={styles.navListItem}>
+                    <Link href="#location"><span className={styles.linkFirstLetter}>L</span>ocations</Link>
                 </li>
             </ul>
         </nav>
