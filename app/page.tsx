219e2db8c219c6288041a5a416ed7fa66ed2c37f
abeonweb <bmospecific@yahoo.com>
2023-03-27 12:08:43 +0100

    include Inter font

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,23 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter} from 'next/font/google'
 import styles from './css/page.module.css'
-import SharedLayout from "./components/SharedLayout"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: '--inter-font' })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <SharedLayout>
-        <div className={styles.description}>
-          <div>
+    <main className={`${styles.homeMain} ${inter.variable}`}>
+        <div className={styles.splashImageContainer}>
             
               <Image
-                src="/fo1.jpg"
-                alt="Couple Image"
-                className={styles.image}
-                width={400}
-                height={500}
+                src="/fo_logo.png"
+                alt="Couple Logo"
+                className={styles.splashImage}
+                width={300}
+                height={300}
                 priority
                 />
-          </div>
         </div>
-    </SharedLayout>
       </main>
   )
 }
