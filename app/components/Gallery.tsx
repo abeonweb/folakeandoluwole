5c74edf857d0a5d06ff64aef765ecf7e0755722e
abeonweb <bmospecific@yahoo.com>
2023-03-29 13:55:27 +0100

    add photos to gallery

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "../css/gallery.module.css";
-
+// import Masonry from "react-responsive-masonry"
 const gallery = () => {
   return (
     <>
       <h1 className={styles.title}>Gallery</h1>
       <div className={styles.container}>
+        {/* <Masonry columnsCount={3}> */}
+
         <Image
           src="/fo1.jpg"
           alt="Couple Image"
@@ -13,7 +15,7 @@ const gallery = () => {
           width={400}
           height={500}
           priority
-        />
+          />
         <Image
           src="/fo2.jpg"
           alt="Couple Image"
@@ -21,7 +23,7 @@ const gallery = () => {
           width={400}
           height={500}
           priority
-        />
+          />
         <Image
           src="/fo3.jpg"
           alt="Couple Image"
@@ -29,7 +31,56 @@ const gallery = () => {
           width={400}
           height={500}
           priority
-        />
+          />
+        <Image
+          src="/fo4.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+        <Image
+          src="/fo5.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+        <Image
+          src="/fo6.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+        <Image
+          src="/fo7.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+        <Image
+          src="/fo8.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+        <Image
+          src="/fo9.jpg"
+          alt="Couple Image"
+          className={styles.image}
+          width={400}
+          height={500}
+          priority
+          />
+          {/* </Masonry> */}
       </div>
     </>
   );
