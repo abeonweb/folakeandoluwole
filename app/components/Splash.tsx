91152addb978dd40ae5f66635ba5f261fb8f3d6c
abeonweb <bmospecific@yahoo.com>
2023-04-02 23:17:44 +0100

    removed awesome reveal from heart images

--- app/components/Splash.tsx
+++ app/components/Splash.tsx
@@ -3,30 +3,29 @@ import React from "react";
 import Image from "next/image";
 import styles from "../css/splash.module.css";
 import WaterWave from "react-water-wave";
-import { AttentionSeeker } from "react-awesome-reveal";
+import { AttentionSeeker, Fade } from "react-awesome-reveal";
 
 const Splash = () => {
   return (
-    // <WaterWave imageUrl={"/fo_empty_bg.png"} 
-    // style={{ width: '100%', height: '100%', backgroundSize: 'cover' }}
-    // >
-    //   {() => (
+    <WaterWave imageUrl={"/fo_empty_bg.png"} 
+    style={{ width: '100%', height: '100%', backgroundSize: 'cover' }}
+    >
+      {() => (
         <section id="splash" className={`${styles.section} ${styles.splashImageContainer}`}>   
-         <AttentionSeeker effect={"pulse"} className={`${styles.heart} ${styles.heart1}`}>
-          <Image  className={styles.heartImage1} src={"/wedding-heart.png"} alt="" width={45} height={45}/>
-         </AttentionSeeker>
-         <AttentionSeeker effect={"pulse"} delay={5000} className={`${styles.heart} ${styles.heart2}`}>
-          <Image className={styles.heartImage2} src={"/wedding-heart.png"} alt="" width={70} height={70}/>
-         </AttentionSeeker>
-         <AttentionSeeker effect={"pulse"} delay={5000} className={`${styles.heart} ${styles.heart3}`}>
-          <Image className={styles.heartImage3} src={"/wedding-heart.png"} alt="" width={100} height={100}/>
-         </AttentionSeeker>
-         <AttentionSeeker effect={"pulse"} delay={3000} className={`${styles.heart} ${styles.heart4}`}>
-          <Image className={styles.heartImage4} src={"/wedding-heart.png"} alt="" width={80} height={90}/>
-         </AttentionSeeker>
+         <Image className={`${styles.heart} ${styles.heartImage1}`} src={"/wedding-heart.png"} alt="" width={45} height={45}/>
+         <Image className={`${styles.heart} ${styles.redHeartImage1}`} src={"/red-wedding-heart2.png"} alt="" width={45} height={45}/>
+         
+          <Image className={`${styles.heart} ${styles.heartImage2}`} src={"/wedding-heart.png"} alt="" width={70} height={70}/>
+          <Image className={`${styles.heart} ${styles.redHeartImage2}`} src={"/red-wedding-heart2.png"} alt="" width={70} height={70}/>
+
+          <Image className={`${styles.heart} ${styles.heartImage3}`} src={"/wedding-heart.png"} alt="" width={100} height={100}/>
+          <Image  className={`${styles.heart} ${styles.redHeartImage3}`} src={"/red-wedding-heart2.png"} alt="" width={100} height={100}/>
+
+          <Image className={`${styles.heart} ${styles.heartImage4}`} src={"/wedding-heart.png"} alt="" width={80} height={90}/>
+          <Image className={`${styles.heart} ${styles.redHeartImage4}`} src={"/red-wedding-heart2.png"} alt="" width={80} height={90}/>
         </section>
-    //   )}
-    // </WaterWave>
+      )}
+    </WaterWave>
   );
 };
 
