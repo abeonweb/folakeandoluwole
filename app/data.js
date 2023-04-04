d578c83b3b4d4bcd8d2893d020a8fa26839ba6b9
abeonweb <bmospecific@yahoo.com>
2023-04-04 22:23:23 +0100

    separate file for data

---
+++ app/data.js
@@ -0,0 +1,15 @@
+const photos = [
+    { id: "1", src: "/fo1.jpg", width: 500, height: 600},
+    { id: "2", src: "/fo2.jpg", width: 500, height: 600},
+    { id: "3", src: "/fo3.jpg", width: 500, height: 600},
+    { id: "4", src: "/fo4.jpg", width: 500, height: 600},
+    { id: "5", src: "/fo5.jpg", width: 500, height: 600},
+    { id: "6", src: "/fo6.jpg", width: 500, height: 600},
+    { id: "7", src: "/fo7.jpg", width: 500, height: 600},
+    { id: "8", src: "/fo8.jpg", width: 500, height: 600},
+    { id: "9", src: "/fo9.jpg", width: 500, height: 600},
+    { id: "10", src: "/london1.jpg", width: 1000, height: 800},
+    { id: "11", src: "/london2.jpg", width: 1000, height: 800},
+  ];
+
+  export default photos
