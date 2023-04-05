3c49331c3dcf30f839827471c732aaaaabece036
abeonweb <bmospecific@yahoo.com>
2023-04-05 23:49:01 +0100

    component to organize info by location

---
+++ app/components/Location.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Location = ({id, title, time, address, mapURL, info}) => {
+  return (
+    <div key={id}>
+        "Location info will be right here"
+    </div>
+  )
+}
+
+export default Location
