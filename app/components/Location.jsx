0527d38e3064f244756a410d88cb470ffa248f48
abeonweb <bmospecific@yahoo.com>
2023-04-07 23:17:34 +0100

    created the card to show each event

--- app/components/Location.jsx
+++ app/components/Location.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Pinyon_Script } from "next/font/google";
+import styles from "../css/location.module.css"
 
+// const pinyon = Pinyon_Script({weight: "400", variable: "--pinyon-font", subsets: ["latin"] })
 const Location = ({id, title, time, address, mapURL, info}) => {
   return (
-    <div key={id}>
-        "Location info will be right here"
+    <div key={id} className={`${styles.location} `}>
+        <h2 className={styles.title}>{title}</h2>
+        <h3>{time}</h3>
+        <p>{address}</p>
+        <p>{info}</p>
+        <p>{mapURL}</p>
     </div>
   )
 }
