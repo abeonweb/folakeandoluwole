5d6dbeb0d5609592759cd591afc37de711c5b4b9
abeonweb <bmospecific@yahoo.com>
2023-04-11 22:18:33 +0100

    image src added via props. iframe style added as obj

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import styles from "../css/location.module.css";
 
 const Map = (props) => {
   return (
-    <div>
+    <div className={styles.map}>
       <iframe
+        src={props.data}
         width="100%"
         height="100%"
-        id="gmap_canvas"
-        src={props.data}
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        style={{border: 0,}}
+        allowFullScreen=""
         loading="lazy"
       ></iframe>
     </div>
