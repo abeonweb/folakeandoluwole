55450d7674c936349f35dacf4ea9606b9da93b5c
abeonweb <bmospecific@yahoo.com>
2023-04-13 22:42:18 +0100

    a function to import images from cloudinary

---
+++ app/utils/cloudinary.js
@@ -0,0 +1,17 @@
+export async function search(){
+    const results = await fetch(
+        `${process.env.CLOUDINARY_URL}/resources/image`,
+        // `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              process.env.CLOUDINARY_API_KEY +
+                ":" +
+                process.env.CLOUDINARY_API_SECRET
+            ).toString("base64")}`,
+          },
+        }
+      ).then((res) => res.json());
+    return results
+}
+
