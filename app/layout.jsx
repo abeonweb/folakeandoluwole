b30e78819e584340fbfe7bed58ed5ce7f677e847
abeonweb <bmospecific@yahoo.com>
2023-04-14 21:55:54 +0100

    include inter font and pass font variable

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,22 @@
 import './globals.css'
 import Navigation from "./components/Navigation"
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: 'Folake and Oluwole',
   description: 'Wedding site for the lovely couple.',
 }
 
-export default function RootLayout({children,}) {
+const inter = Inter({ subsets: ["latin"], variable: "--inter-font" });
+
+export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${inter.variable}`}>
         <Navigation/>
         {children}
         </body>
     </html>
   )
 }
+
